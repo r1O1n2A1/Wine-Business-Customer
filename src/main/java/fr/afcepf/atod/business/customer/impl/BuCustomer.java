ba9abd0232a80a7bb5443aa2f26bc2e9092336ec
Metabeen <Metabeen@DESKTOP-BC1N5VT>
2017-03-26 20:35:02 +0200

    Mise à jour du business Customer (javadoc + todo)

--- src/main/java/fr/afcepf/atod/business/customer/impl/BuCustomer.java
+++ src/main/java/fr/afcepf/atod/business/customer/impl/BuCustomer.java
@@ -5,72 +5,82 @@
  */
 package fr.afcepf.atod.business.customer.impl;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.afcepf.atod.business.customer.api.IBuCustomer;
 import fr.afcepf.atod.customer.data.api.IDaoCustomer;
-import fr.afcepf.atod.customer.data.impl.DaoCustomer;
 import fr.afcepf.atod.vin.data.exception.WineErrorCode;
 import fr.afcepf.atod.vin.data.exception.WineException;
 import fr.afcepf.atod.wine.entity.Customer;
 import fr.afcepf.atod.wine.entity.User;
 
 /**
- *
- * @author ronan
+ * Class permettant la gestion du client de 
+ * l'application. Implémentation de l'interface
+ * business customer.
+ * @author ronan - metabeen
  */
 @Service
 public class BuCustomer implements IBuCustomer {
+	/**
+	 * Gestion de l'injection via Spring par
+	 * l'annotation autowired. 
+	 */
+	@Autowired
+	private IDaoCustomer daoCustomer;
+	/**
+	 * Logger de log 4j pour les codes d'erreur.
+	 */
+	private Logger log = Logger.getLogger(BuCustomer.class);
 
-    @Autowired
-    private IDaoCustomer daoCustomer;
-
-    
-    @Override
-    public User connect(String mail, String password) throws WineException {
-        User user = null;
-        WineException wineException = null;
-        try {
-            if (!mail.equalsIgnoreCase("")
-                    && !password.equalsIgnoreCase("")) {
-                user = daoCustomer.connect(mail, password);
-                if (!user.getLastname().equalsIgnoreCase("")) {
-                    // return user;
-                } else {
-                    wineException = new WineException(
-                            WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
-                            mail + " - not found in the db");
-                }
-            } else {
-                wineException = new WineException(
-                        WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
-                        mail + " or " + password
-                        + " -  invalid");
-            }
-        } catch (Exception e) {
-            wineException = new WineException(
-                    WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
-                    mail + " or " + password
-                    + " -  invalid");
-        }
-        if (wineException != null) {
-            throw wineException;
-        }
-        return user;
-    }
-
+	@Override
+	public User connect(String mail, String password) throws WineException {
+		User user = null;
+		WineException wineException = null;
+		try {
+			if (!mail.equalsIgnoreCase("")
+					&& !password.equalsIgnoreCase("")) {
+				user = daoCustomer.connect(mail, password);
+				if (!user.getLastname().equalsIgnoreCase("")) {
+					//TODO return user
+				} else {
+					wineException = new WineException(
+							WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
+							mail + " - not found in the db");
+				}
+			} else {
+				wineException = new WineException(
+						WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
+						mail + " or " + password
+						+ " -  invalid");
+			}
+		} catch (Exception e) {
+			wineException = new WineException(
+					WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
+					mail + " or " + password
+					+ " -  invalid");
+			log.info(e);
+		}
+		if (wineException != null) {
+			throw wineException;
+		}
+		return user;
+	}
+	@Override
 	public User findUserById(int id) throws WineException {
 		return daoCustomer.findObj(id);
-		
+
 	}
+	@Override
 	public Customer addNewCustomer(Customer customer) {
 		try {
 			daoCustomer.insertObj(customer);
 		} catch (WineException e) {
-			e.printStackTrace();
+			log.info(e);
 		}
 		return customer;		
 	}
-    
+
 }
