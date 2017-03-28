b067ea39ce9536099fe7cd6c48eb4585064bfb89
metabeen <cyril.deschamps88@gmail.com>
2017-03-28 14:25:53 +0200

    Update jdoc

--- src/main/java/fr/afcepf/atod/business/customer/api/IBuCustomer.java
+++ src/main/java/fr/afcepf/atod/business/customer/api/IBuCustomer.java
@@ -29,26 +29,27 @@ public interface IBuCustomer {
 	 */
 	User connect(String mail, String password) throws WineException;
 	/**
-	 * M�thode permettant de r�cup�rer un {@link User} dans l'unit� de persistence.
-	 * @param id Param�tre de type int.
+	 * Méthode permettant de récupérer un {@link User} dans l'unité de persistence.
+	 * @param id Paramètre de type int.
 	 * @return un {@link User}.
 	 * @throws WineException 
 	 * <ul>
-	 * <li>L'identifiant en argument ne correspond � aucun utilisateur
-	 * dans l'unit� de persistence,</li>
-	 * <li>les contraintes de saisies ne sont pas respect�es,</li>
-	 * <li>l'unit� de persistence est inaccessible.</li>
+	 * <li>L'identifiant en argument ne correspond à aucun utilisateur
+	 * dans l'unité de persistence,</li>
+	 * <li>les contraintes de saisies ne sont pas respectées,</li>
+	 * <li>l'unité de persistence est inaccessible.</li>
 	 * </ul>
 	 */
 	User findUserById(int id) throws WineException;
 	/**
-	 * M�thode permettant l'ajout d'un {@link User} dans l'unit� de persistence.
-	 * @param customer un {@link Customer} ajout� dans la base de donn� avec son identifiant.
+	 * Méthode permettant l'ajout d'un {@link User} dans l'unité de persistence.
+	 * @param customer un {@link Customer} ajouté dans l'unité de persistence
+	 *  avec son identifiant.
 	 * @return un {@link Customer}.
 	 * @throws WineException 
 	 * <ul>
-	 * <li>Les contraintes de saisies ne sont pas respect�es,</li>
-	 * <li>l'unit� de persistence est inaccessible.</li>
+	 * <li>Les contraintes de saisies ne sont pas respectées,</li>
+	 * <li>l'unité de persistence est inaccessible.</li>
 	 * </ul>
 	 */
 	Customer addNewCustomer(Customer customer) throws WineException;  
